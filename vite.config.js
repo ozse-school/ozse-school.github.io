41ca6790308241158c4fac7b21b34e9f5033f806
kanin-kearpimy <kanin.kearpimy@gmail.com>
2025-06-19 23:22:35 +1000

    full path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || '/osze.github.io',
+  base: process.env.VITE_BASE_PATH || 'https://ozse-school.github.io/osze.github.io',
   build: {
     outDir: 'dist',
   }

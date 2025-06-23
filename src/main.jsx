de8d301e54f087328cd14de9b0dd11c15f4d109c
kanin-kearpimy <kanin.kearpimy@gmail.com>
2025-06-23 10:25:32 +1000

    adjust base path

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={'/osze-school.github.io'}>
+    <BrowserRouter basename={'/'}>
       <App />
     </BrowserRouter>
   </React.StrictMode>

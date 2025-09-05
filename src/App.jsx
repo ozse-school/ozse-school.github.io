8f40a102857d3c344573ab2f70f2546920b627b8
kanin-kearpimy <kanin.kearpimy@gmail.com>
2025-09-05 23:07:47 +1000

    remove registration page. use the melbourne link instead.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import PageFrame from './components/PageFrame';
 import HomePage from './HomePage';
 import HistoryPage from "./HistoryPage";
 import OCTeamPage from "./OCTeamPage.jsx";
-import RegistrationPage from "./RegistrationPage/RegistrationPage.jsx";
 
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/history" element={<HistoryPage />} />
         <Route path="/team" element={<OCTeamPage />} />
-        <Route path="/registration" element={<RegistrationPage />} />
       </Route>
     </Routes>
   );
